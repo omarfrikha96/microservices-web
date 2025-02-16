25784d04e98e64608be088424228386ae064fc4c
omarfrikha96 <omar.frikha96@gmail.com>
2025-02-16 16:05:44 +0100

    implement circuit breaker resilience4j

--- api-gateway/src/main/java/com/omarfr/microservices/gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/omarfr/microservices/gateway/routes/Routes.java
@@ -4,13 +4,18 @@ import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctio
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import java.net.URI;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions.circuitBreaker;
 import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import static org.springframework.http.HttpStatus.*;
 import static org.springframework.web.servlet.function.RequestPredicates.path;
 
 @Configuration
@@ -20,6 +25,7 @@ public class Routes {
     public RouterFunction<ServerResponse> productServiceRoute() {
         return route("product_service")
                 .route(path("/api/product"), http("http://localhost:8080"))
+                .filter(circuitBreaker("productServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .build();
     }
 
@@ -27,6 +33,7 @@ public class Routes {
     public RouterFunction<ServerResponse> productServiceSwaggerRoute() {
         return GatewayRouterFunctions.route("product_service_swagger")
                 .route(RequestPredicates.path("/aggregate/product-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8080"))
+                .filter(circuitBreaker("productServiceSwaggerCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .filter(setPath("/v3/api-docs"))
                 .build();
     }
@@ -35,6 +42,7 @@ public class Routes {
     public RouterFunction<ServerResponse> orderServiceRoute() {
         return route("order_service")
                 .route(path("/api/order"), http("http://localhost:8081"))
+                .filter(circuitBreaker("orderServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .build();
     }
 
@@ -42,6 +50,7 @@ public class Routes {
     public RouterFunction<ServerResponse> orderServiceSwaggerRoute() {
         return GatewayRouterFunctions.route("order_service_swagger")
                 .route(RequestPredicates.path("/aggregate/order-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8081"))
+                .filter(circuitBreaker("orderServiceSwaggerCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .filter(setPath("/v3/api-docs"))
                 .build();
     }
@@ -50,6 +59,7 @@ public class Routes {
     public RouterFunction<ServerResponse> inventoryServiceRoute() {
         return route("inventory_service")
                 .route(path("/api/inventory"), http("http://localhost:8082"))
+                .filter(circuitBreaker("inventoryServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .build();
     }
 
@@ -57,9 +67,18 @@ public class Routes {
     public RouterFunction<ServerResponse> inventoryServiceSwaggerRoute() {
         return GatewayRouterFunctions.route("inventory_service_swagger")
                 .route(RequestPredicates.path("/aggregate/inventory-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8082"))
+                .filter(circuitBreaker("inventoryServiceSwaggerCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .filter(setPath("/v3/api-docs"))
                 .build();
     }
 
+    @Bean
+    public RouterFunction<ServerResponse> fallbackRoute() {
+        return route("fallbackRoute")
+                .GET("/fallbackRoute", request ->
+                        ServerResponse.status(SERVICE_UNAVAILABLE)
+                                .body("Service Unavailable, please try again later"))
+                .build();
+    }
 
 }
--- order-service/src/main/java/com/omarfr/microservices/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/omarfr/microservices/order/config/RestClientConfig.java
@@ -4,12 +4,16 @@ import com.omarfr.microservices.order.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
+
     @Value("${inventory.url}")
     private String inventoryServiceUrl;
 
@@ -17,9 +21,17 @@ public class RestClientConfig {
     public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .requestFactory(getClientRequestFactory())
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+
+    private ClientHttpRequestFactory getClientRequestFactory() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout((int) Duration.ofSeconds(3).toMillis());
+        factory.setReadTimeout((int) Duration.ofSeconds(3).toMillis());
+        return factory;
     }
 }

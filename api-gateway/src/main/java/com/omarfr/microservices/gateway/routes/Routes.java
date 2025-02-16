0f07bbe4aaaf3d8e6d6e3a2c0e4ebfe41e97397a
omarfrikha96 <omar.frikha96@gmail.com>
2025-02-16 11:51:23 +0100

    add keycloak security

--- api-gateway/src/main/java/com/omarfr/microservices/gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/omarfr/microservices/gateway/routes/Routes.java
@@ -4,9 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
-
-import static java.net.URI.create;
-import static org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions.circuitBreaker;
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 import static org.springframework.web.servlet.function.RequestPredicates.path;
